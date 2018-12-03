d75c5b9c6c6cfa560334145e1b7f34d345c3181e
medved_v_ushanke <getingjagare@gmail.com>
2018-12-03 15:43:48 +0300

    send utm to crm

--- app/helpers.php
+++ app/helpers.php
@@ -230,6 +230,8 @@ function phone_format($phone, $format, $mask = '#')
 function sendToCRM($fields)
 {
 
+	var_dump($fields);
+
     $fields['entryPoint'] = 'MeetingsFromSite_simstore';
     $fields['key'] = 'C2Dq9Wx70DhxnjWJ3Aq8uNpF7sx9SvNvCdVd';
     //$fields['dev'] = config('app.debug') ? 'true' : null;
